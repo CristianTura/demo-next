32f6a67c74309f60646df628f5a326a1e026ad42
CristianTura <cristianbuenaventura15@gmail.com>
2022-03-31 12:38:49 -0500

    first commit

---
+++ pages/contact/index.jsx
@@ -0,0 +1,24 @@
+import Head from "next/head";
+import Link from "next/link";
+import { MainLayout } from "../../components/layouts/MainLayout";
+
+
+export default function ContactPage() {
+  return (
+    <MainLayout>
+      <h1>Contact Page</h1>
+
+      <h1 className={'title'}>
+        Ir a{" "}
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+      </h1>
+
+      <p className={'description'}>
+        Get started by editing{" "}
+        <code className={'code'}>contact/about.js</code>
+      </p>
+    </MainLayout>
+  );
+}
